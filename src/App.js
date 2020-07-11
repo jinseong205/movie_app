784f4e29b76c9744d3ba9b15e69beee1ce955bff
Jinseong <Jinseong@Jinseong.local>
2020-07-11 18:09:59 +0900

    js commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
+
+//data from server
+const foodLike = [
+  { name: 'kimchi', image: 'image' },
+  { name: 'samgyeopsal', image: 'image' },
+  { name: 'bibimbap', image: 'image' },
+  { name: 'doncasu', image: 'image' },
+  { name: 'Kimbap', image: 'image' }
+
+];
+
+
 function App() {
   return (
     <div className="App">
-      <h1>hello!</h1>
-
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
-
-
+      {foodLike.map(dish => (<Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
 
-function Food(props) {
-  return <h1>I like {props.fav}</h1>
+function Food({ name,picture }) {
+  return (
+    <div>
+    <h2>I like {name}</h2>
+    <h2>{picture}**</h2>   
+    {/* <img src = {picture} */}
+
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Food.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+
+function Food({name}) {
+    return <h1>I like {name}</h1>;
+  }
+  
+  export default App;
+  
