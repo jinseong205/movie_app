dc06b49db6bece291044c92417dc5a4cd2cf61ee
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-01 23:02:27 +0900

    test
    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-function App() {
+import React from 'react';
+
+function Food({name, picture}){
   return (
-    <div className="App">
-      Hello!
+    <div>
+      <h1>I like {name}</h1>
+      <img src = {picture}/>
     </div>
   );
 }
 
+const foodILike = [
+  {
+    name : 'kimchi',
+    image: '123',
+  },
+  {
+    name : 'Samgyeopsal',
+    image: '234',
+  },
+  {
+    name : 'Bibimbap',
+    image: '3456',
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      {foodILike.map(dish => 
+        <Food name={dish.name} picture={dish.image}/>
+        )}
+    </div>
+  );
+}
+
+
+//App.js > index.js
 export default App;
