a537595655b748c91a75e38004266d8f60fd0759
jinseong205 <jinseong205@gmail.com>
2021-08-03 22:07:07 +0900

    props test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name, picture}){
+function Food({key, name, picture, rating}){
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src = {picture}/>
+      <h4>{rating}/5.0</h4>
+      <img src = {picture} alt ={key}/>
     </div>
   );
 }
 
 const foodILike = [
   {
+    id : 1,
     name : 'kimchi',
     image: '123',
+    rating : 5
   },
   {
+    id: 2,
     name : 'Samgyeopsal',
     image: '234',
+    rating : 10
   },
   {
+    id :3,
     name : 'Bibimbap',
     image: '3456',
+    rating : 8
   },
 ];
 
@@ -28,12 +36,17 @@ function App() {
   return (
     <div className="App">
       {foodILike.map(dish => 
-        <Food name={dish.name} picture={dish.image}/>
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
         )}
     </div>
   );
 }
 
+Food.propTypes = {
+  name : PropTypes.string.isRequired,  
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number,
+};
 
 //App.js > index.js
 export default App;
