f4cda4019ab7c39cc93c05935c83c026c7cb3239
jinseong205 <jinseong205@gmail.com>
2021-08-23 21:58:36 +0900

    movie app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
 
   state = {
-    count: 0
-  };
-  
-  add = () => {
-    this.setState({count : this.state.count + 1});
-  };
-  minus = () => {
-    this.setState({count : this.state.count -1});
+    isLoading:true,
+    movies:[],
   };
 
+  getMovies = async() =>{
+    const {
+      data : {
+        data : {movies},
+      },
+    } =  await axios.get("http://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+
+    this.setState({movies, isLoading : false});
+  }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
   render() {
+    const {isLoading} = this.state;
+    
     return (
     <div>
-      <h1> The number is : {this.state.count}</h1>
-      <button onClick = {this.add}>add</button>
-      <button onClick = {this.minus}>minus</button>
+      {isLoading ? 'Loading...': 'We are ready'}
     </div>
     );
   }
